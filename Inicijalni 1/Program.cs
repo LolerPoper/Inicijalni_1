76a3e3bd1be6ea10c744d6c335cc3d65c44ed77a
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-09-11 10:30:49 +0200

    Add files via upload

---
+++ Inicijalni 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100, b = 3, x;
+            double y;
+
+            x = a / b;
+            y = a % b;
+
+            Console.WriteLine("a dijeljeno s b je " + x + " i ostatak " + y);
+            Console.ReadKey();
+
+        }
+    }
+}
